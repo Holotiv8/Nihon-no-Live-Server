41d520ce043c88a7e1e2b62f3c15dd351d7f15c7
flamousz <valflamous@gmail.com>
2023-02-09 01:41:50 +0700

    change typo lano

--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const UserController = require("../controllers/userContoller");
+const UserController = require("../controllers/UserContoller");
 const authentication = require("../middlewares");
 const router = express.Router();
 
