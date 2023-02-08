62fc40c42f53379e3f4f23cb4a2c20cdbbfc6f84
BintangMy <d2maw5@gmail.com>
2023-02-08 21:35:46 +0700

    change api

--- server/helpers/spotify.js
+++ server/helpers/spotify.js
@@ -6,7 +6,7 @@ const spotifyFunction = async (spotifyId) => {
     url: "https://spotify23.p.rapidapi.com/artist_singles/",
     params: { id: spotifyId, offset: "0", limit: "20" },
     headers: {
-      "X-RapidAPI-Key": "4d7ec34579msh01e41d823cb9416p13d6e6jsne35744429b63",
+      "X-RapidAPI-Key": "e1eba056a8mshdb43c782b6c0609p1e7846jsn2de07ec09145",
       "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
     },
   };
--- server/helpers/youtubeLive.js
+++ server/helpers/youtubeLive.js
@@ -6,7 +6,7 @@ const youtubeLiveFunction = async (youtubeId) => {
     url: "https://youtube138.p.rapidapi.com/channel/videos/",
     params: { id: youtubeId, filter: "live_now", hl: "en", gl: "US" },
     headers: {
-      "X-RapidAPI-Key": "cca1deff66mshb630dfb5df4a4cfp13d61cjsn01b1cc2f9813",
+      "X-RapidAPI-Key": "e1eba056a8mshdb43c782b6c0609p1e7846jsn2de07ec09145",
       "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
   };
